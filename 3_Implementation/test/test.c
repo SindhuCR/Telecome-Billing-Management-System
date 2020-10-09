1ea559d2d10ccec73a5daf5e059153288a39b86c
stepin104482 <69038575+stepin104482@users.noreply.github.com>
2020-10-09 20:57:21 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,1 +1,30 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+#include<telecom.h>
+#define PROJECT_NAME    "Telecom_Billing_Management_System"
+
+
+
+
+
+int main() {
+
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+  
+
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+
+
+  CU_basic_run_tests();
+
+
+  CU_cleanup_registry();
+
+  return 0;
+}
+
+
+
 
